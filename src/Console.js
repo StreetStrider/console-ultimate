465d3a65c0c17d4979a813d49297ee378816c3a7
StreetStrider <teamfortresslife@gmail.com>
2015-03-15 19:50:29 +0300

    console.dir.

--- src/Console.js
+++ src/Console.js
@@ -41,6 +41,8 @@
 var
 	NL = '\n',
 
+	slice = Array.prototype.slice,
+
 	util = require('util'),
 
 	colr = require('cli-color'),
@@ -49,7 +51,8 @@ var
 	yellow = colr.yellow,
 
 	inst = require('aux.js/inst'),
-	prop = require('aux.js/prop');
+	prop = require('aux.js/prop'),
+	extend = require('aux.js/object/extend');
 
 var Console = module.exports = function Console (stdout, stderr)
 {
@@ -88,3 +91,113 @@ function format (items)
 {
 	return util.format.apply(null, items);
 }
+
+
+Console.prototype.dir = (function ()
+{
+
+var dir = function (object, options /* flags */)
+{
+	if (arguments.length === 1)
+	{
+		options = doDefaults();
+	}
+	else if (isNodeLike(arguments))
+	{
+		options = doDefaults(options);
+	}
+	else
+	{
+		/* extended behavior: flags */
+		var
+			eff   = doDefaults(),
+			flags = slice.call(arguments, 1);
+
+		flags.forEach(doFlag(eff));
+
+		options = eff;
+	}
+
+	this._stdout.write(util.inspect(object, options) + NL);
+}
+
+
+function doDefaults (options)
+{
+	if (! options)
+	{
+		return extend({}, defaults);
+	}
+	else
+	{
+		return extend({}, defaults, options);
+	}
+}
+
+var defaults =
+{
+	customInspect: false,
+	colors: true, /* for conformance with log, info, warn, error above,
+	later it will be customizeable via Console(_, _, options) */
+
+	/* Node defaults: */
+	// showHidden: false,
+	// depth: 2,
+	// colors: false,
+	// customInspect: true,
+}
+
+function isNodeLike (A)
+{
+	return (A.length === 2) && (Object(A[1]) === A[1]);
+}
+
+function doFlag (options)
+{
+	return function (flag)
+	{
+		switch (typeof flag)
+		{
+		case 'object':
+			extend(options, flag);
+			break;
+
+		case 'number':
+			extend(options, { depth: flag });
+			break;
+
+		case 'string':
+			stringFlag(options, flag);
+			break;
+
+		}
+	}
+}
+
+function stringFlag (options, flag)
+{
+	switch (flag)
+	{
+	case 'colors':
+		extend(options, { colors: true });
+		break;
+
+	case 'nocolors':
+		extend(options, { colors: false });
+		break;
+
+	case 'showHidden':
+		extend(options, { showHidden: true });
+		break;
+
+	case 'customInspect':
+		extend(options, { customInspect: true });
+		break;
+
+	}
+}
+
+
+return dir;
+
+}());
--- test.js
+++ test.js
@@ -15,3 +15,26 @@ c.warn('%s/%s', 'b', 1);
 
 c.error('a', 'b', 'c');
 c.error('%s/%s', 'b', 1);
+
+var tree = { x: 1, y: 2, 3: { 3: { 3: 'tree' }}};
+
+console.log('');
+console.log('depth: 0');
+c.dir(tree, 0);
+console.log('');
+
+console.log('depth: 1');
+c.dir(tree, 1);
+console.log('');
+
+console.log('depth: default');
+c.dir(tree);
+console.log('');
+
+console.log('nocolors');
+c.dir(tree, 'nocolors');
+console.log('');
+
+console.log('std options');
+c.dir(tree, { depth: 0, colors: true, showHidden: true });
+console.log('');
