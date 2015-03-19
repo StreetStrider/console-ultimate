4b8bbc084c480d54c5c7707ea9f992a1f516e162
StreetStrider <teamfortresslife@gmail.com>
2015-03-19 20:11:46 +0300

    example: dir.

---
+++ examples/dir.js
@@ -0,0 +1,44 @@
+
+
+
+var
+	Console = require('..'),
+	console = Console(),
+
+	log = console.log;
+
+var
+	prop = require('aux.js/prop').value;
+
+var object =
+{
+	x: 1,
+	y: [ 1, 2, 3 ],
+	sub:
+	{
+		x: 2,
+		y: 'y',
+		sub2:
+		{
+			sub3: {},
+			z: 'z'
+		}
+	}
+}
+
+prop(object, 'hidden', 42);
+prop(object.sub, 'hidden2', 'secret');
+
+log('dir,');
+log('console.dir(object);');
+console.dir(object);
+log('');
+
+log('dir with Node util.inspect options,')
+log('console.dir(object, { depth: 1, showHidden: true });')
+console.dir(object, { depth: 1, showHidden: true });
+log('');
+
+log('dir with flags,');
+log('console.dir(object, 1, \'showHidden\');');
+console.dir(object, 1, 'showHidden');
