eccda1f34f944dbd38a1c97b71248bc1f54f1a3c
Strider <strange.transistor@gmail.com>
2019-02-09 03:24:33 +0300

    basic Log.

--- start.js
+++ start.js
@@ -1,8 +1,14 @@
 
-// import './'
+/* import './' */
 require('./')
 
-console.log({ x: 1 })
+var X = { x: 1, y: 2, z: [ 1, '2', true ]}
 
-// import c from './console'
-console.log(console === require('./console').default)
+console.log(X)
+console.warn(X)
+console.info(X)
+console.dir(X)
+
+console.log('%s %d %j', 1, true, {})
+
+// console.error(X)
