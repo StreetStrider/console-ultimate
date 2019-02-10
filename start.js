5700c41fce64318bda63303d42fd0b508bc80810
Strider <strange.transistor@gmail.com>
2019-02-10 03:55:06 +0300

    distinct log settings.

--- start.js
+++ start.js
@@ -7,10 +7,19 @@ var X = { x: 1, y: 2, z: [ 1, '2', true ]}
 console.log(X)
 console.warn(X)
 console.info(X)
-console.dir(X)
 
-console.log('%s %d %j', 1, true, { x: 5 })
-console.log('%s %d', 1, true, { x: 5 })
-console.log('%s %d %j', 1, true)
+console.log('foo bar')
+console.warn('foo bar')
+console.info('foo bar')
+
+console.dir(X)
+console.dir('foo bar')
+console.dir(console.dir)
+
+// console.log('%s %d %j', 1, true, { x: 5 })
+// console.log('%s %d', 1, true, { x: 5 })
+// console.log('%s %d %j', 1, true)
 
 // console.error(X)
+
+console.log(console)
