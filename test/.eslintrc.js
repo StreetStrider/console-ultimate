6ee46787b26d18f0d97f328dffaaf4181d3a48ad
Strider <strange.transistor@gmail.com>
2019-02-10 22:55:44 +0300

    eslint + outlander.

---
+++ test/.eslintrc.js
@@ -0,0 +1,19 @@
+
+module.exports =
+{
+	env:
+	{
+		mocha: true,
+	},
+
+	globals:
+	{
+		expect: true,
+	},
+
+	rules:
+	{
+		// 'no-unused-expressions': 0,
+		// 'max-statements': [ 1, 30 ],
+	}
+}
