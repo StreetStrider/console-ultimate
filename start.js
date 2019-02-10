2a65fe70e699ddfb9eb0c905e2144620e4daa5ba
Strider <strange.transistor@gmail.com>
2019-02-11 02:44:40 +0300

    depth = 1.

--- start.js
+++ start.js
@@ -2,9 +2,13 @@
 /* import './' */
 require('./')
 
-console.dir(console)
+// console.dir(console)
 
-console.log({ x: 17, y: true, z: [ null, NaN, /xyz/ ] })
-console.error({ x: 17, y: true, z: [ null, NaN, /xyz/ ] })
+// console.log({ x: 17, y: true, z: [ null, NaN, /xyz/ ] })
 
-console.error(new Error('X'))
+// console.log({ x: 17, y: true, z: [ null, NaN, /xyz/ ] })
+console.log(console)
+
+// console.error({ x: 17, y: true, z: [ null, NaN, /xyz/ ] })
+
+// console.error(new Error('X'))
