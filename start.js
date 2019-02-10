e91988cefa02f10442c6cdbc67114ad079d62e66
Strider <strange.transistor@gmail.com>
2019-02-10 22:53:26 +0300

    improve styling.

--- start.js
+++ start.js
@@ -2,6 +2,8 @@
 /* import './' */
 require('./')
 
-var e = new Error('foo')
+console.dir(console)
 
-console.error(e)
+console.log({ x: 17, y: true, z: [ null, NaN, /xyz/ ] })
+
+console.error(new Error('X'))
