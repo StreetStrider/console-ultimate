8d4d9d809963f0fd5d8e06950e2a6df4121ae150
Strider <strange.transistor@gmail.com>
2019-02-10 21:21:30 +0300

    test meta.

--- test/console.test.js
+++ test/console.test.js
@@ -5,22 +5,41 @@ import cat from './cat'
 
 describe('console', () =>
 {
-	it('log', async () =>
+	it_console(
 	{
-		await test_console({}, ' ⚫ 1\n', (console) =>
-		{
-			console.log(1)
-		})
 	})
-	it('colors = true, tty = false', async () =>
+
+	it_console(
 	{
-		await test_console({ colors: true }, '\u001b[0m ⚫ \u001b[33m1\u001b[39m\u001b[0m\n',
-		(console) =>
+		title: 'log',
+		output: ' ⚫ 1\n',
+		test (console)
 		{
 			console.log(1)
-		})
+		},
+	})
+
+	it_console(
+	{
+		title: 'colors = true, tty = false',
+		options: { colors: true },
+		output: '\u001b[0m ⚫ \u001b[33m1\u001b[39m\u001b[0m\n',
+		test (console)
+		{
+			console.log(1)
+		},
 	})
 })
+
+function it_console ({ title, options = {}, output, test })
+{
+	if (! title) { return }
+
+	it(title, async () =>
+	{
+		await test_console(options, output, test)
+	})
+}
 
 async function test_console (options, output, test)
 {
