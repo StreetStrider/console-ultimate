9c7a8c4ad6f09437551ef3bd6429ce7ec3712359
Strider <strange.transistor@gmail.com>
2019-02-10 19:12:32 +0300

    error and stack.

--- start.js
+++ start.js
@@ -2,27 +2,6 @@
 /* import './' */
 require('./')
 
-var X = { x: 1, y: 2, z: [ 1, '2', true ]}
+var e = new Error('foo')
 
-Promise.resolve(X)
-.then(console.log._)
-.then(console.warn._)
-.then(console.info._)
-.then(console.dir._)
-
-console.log('foo bar')
-console.warn('foo bar')
-console.info('foo bar')
-
-console.dir(X)
-console.dir('foo bar')
-
-console.dir(console.dir)
-
-// console.log('%s %d %j', 1, true, { x: 5 })
-// console.log('%s %d', 1, true, { x: 5 })
-// console.log('%s %d %j', 1, true)
-
-// console.error(X)
-
-console.log(console)
+console.error(e)
