75979962edee0cef22ac5db82a9f6a0b06d1d907
Strider <strange.transistor@gmail.com>
2019-02-13 01:53:41 +0300

    experimenting with indentation.

--- start.js
+++ start.js
@@ -4,21 +4,20 @@ require('./')
 
 var X = { x: 17, y: true, z: [ null, NaN, /xyz/ ] }
 
+console.dir(Object.keys(global))
 console.group('Group')
-// console.dir(Object.keys(global))
+console.dir(Object.keys(global))
 
-// console.group()
+console.group()
 console.log(X)
 console.info(X)
 process.stdout.write('YYY\n')
 console.warn(X)
 
-// console.error(X)
-// console.error(new Error('X'))
+console.error(X)
+console.error(new Error('X'))
 console.groupEnd() //<
 
-// console.error(new Error('X'))
-// console.dir(new Error('X'))
-// console.groupEnd() //<
-
-// console.log('Spy', spy)
+console.error(new Error('X'))
+console.dir(new Error('X'))
+console.groupEnd() //<
