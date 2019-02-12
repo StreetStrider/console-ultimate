a14f656d220c89af0a7bda10409b9ec2158365e6
Strider <strange.transistor@gmail.com>
2019-02-12 19:35:25 +0300

    experiment with stream spy.

--- start.js
+++ start.js
@@ -4,13 +4,33 @@ require('./')
 
 var X = { x: 17, y: true, z: [ null, NaN, /xyz/ ] }
 
+var stdout = process.stdout
+
+var spy = []
+
+stdout._write = ((stream, $write) =>
+{
+	return function write (...args)
+	{
+		spy.push(args[0])
+
+		$write.apply(stream, args)
+	}
+})
+(stdout, stdout._write)
+
+// console.log(stdout._write)
+// console.log(stdout._writev)
+
 console.dir(Object.keys(global))
 
 console.log(X)
-console.info(X)
-console.warn(X)
+// console.info(X)
+// console.warn(X)
 
-console.error(X)
-console.error(new Error('X'))
+// console.error(X)
+// console.error(new Error('X'))
 
-console.dir(new Error('X'))
+// console.dir(new Error('X'))
+
+console.log('Spy', spy)
