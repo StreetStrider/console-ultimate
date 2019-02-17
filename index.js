becb4e91544cee020eef151134f825cbcd1386fb
Strider <strange.transistor@gmail.com>
2019-02-17 17:34:28 +0300

    esm-igniter for non-esm users.

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 
-import value from './console'
+require = require('esm')(module)
+
+var value = module.exports = require('./console').default
 
 Object.defineProperty(global, 'console',
 {
 	value,
 	configurable: true,
 })
-
-export default value
