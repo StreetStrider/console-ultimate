475f6b94b207fd138b6aa38ebe14623a07fc3d9d
Strider <strange.transistor@gmail.com>
2019-02-19 00:27:20 +0300

    workon col width.

--- start.js
+++ start.js
@@ -2,7 +2,7 @@
 /* import './' */
 require('./')
 
-var X = { x: 17, y: true, z: [ null, NaN, /xyz/ ] }
+var X = { x: 17, x2: 1, y: true, z: [ null, NaN, /xyz/ ] }
 var Y = [ X, X, X ]
 
 // console.log(Y)
