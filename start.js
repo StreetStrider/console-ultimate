bf651b394fdabec09dda09f4e7c037d4e897966f
Strider <strange.transistor@gmail.com>
2019-02-20 16:32:31 +0300

    collect keys.

--- start.js
+++ start.js
@@ -2,8 +2,16 @@
 /* import './' */
 require('./')
 
-var X = { x: 17, x2: 1, y: true, z: [ null, NaN, /xyz/ ] }
-var Y = [ X, X, X ]
+// var X = { x: 17, x2: 1, y: true, z: [ null, NaN, /xyz/ ] }
+// var Y = [ X, X, X ]
 
 // console.log(Y)
-console.table(Y)
+// console.table(Y)
+console.table(
+[
+	{ x: 1, y: 2 },
+	{ y: 2, z: 3 },
+	{ x: 1, z: 3 },
+	// [ 17 ],
+	// 17,
+])
