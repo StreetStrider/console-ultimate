5ed52f764e68aa3a75e9807391885bab4efc0394
Strider <strange.transistor@gmail.com>
2019-02-20 17:36:24 +0300

    object table key.

--- start.js
+++ start.js
@@ -6,12 +6,23 @@ require('./')
 // var Y = [ X, X, X ]
 
 // console.log(Y)
-// console.table(Y)
+
+console.table(17)
+
 console.table(
 [
 	{ x: 1, y: 2 },
 	{ y: 2, z: 3 },
+	17,
 	{ x: 1, z: 3 },
 	[ 17 ],
-	17,
 ])
+
+console.table(
+{
+	1: { x: 1, y: 2 },
+	2: { y: 2, z: 3 },
+	25: 17,
+	5: { x: 1, z: 3 },
+	7: [ 17 ],
+})
