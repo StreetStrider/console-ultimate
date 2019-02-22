3fbc33d3ee0fa5752b4f471b2852952e70033a0c
Strider <strange.transistor@gmail.com>
2019-02-22 19:17:07 +0300

    table: test.

--- start.js
+++ start.js
@@ -5,26 +5,14 @@ require('./')
 // var X = { x: 17, x2: 1, y: true, z: [ null, NaN, /xyz/ ] }
 // var Y = [ X, X, X ]
 
-// console.table(17)
-
-console.table(
-[
-	{ x: 1, y: 2 },
-	{ y: 2, z: 3 },
-	17,
-	{ x: 1, z: 3 },
-	[ 17 ],
-	[[ 17 ]],
-])
-
 console.table(
 {
 	1: { x: 1, y: 2 },
 	2: { y: 2, z: 3 },
-	4: 17,
-	5: 'aasdas\ndasdas',
-	15: { x: 1, z: 3 },
-	16: Symbol('foo'),
-	25: [ 17 ],
-	27: [[ 17 ]],
+	3: 17,
+	10: { x: 1, z: 3 },
+	4: [ 17 ],
+	20: [[ 17 ]],
+	25: { x: 1, a: [] },
+	21: Symbol('S'),
 })
