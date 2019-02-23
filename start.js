66e6b2ba648c3ae69fe16bac747b83183085f687
Strider <strange.transistor@gmail.com>
2019-02-24 02:06:01 +0300

    check imports.

--- start.js
+++ start.js
@@ -1,7 +1,7 @@
 
-// import Console from './Console'; var console = Console()
+// import Console from './Console'; var console = Console({ colors: false })
 // import console from './console'
-// import upgrade from './upgrade'; upgrade({})
+// import upgrade from './upgrade'; upgrade({ colors: false })
 import '.'
 
 // var X = { x: 17, x2: 1, y: true, z: [ null, NaN, /xyz/ ] }
