aeb0c9798c47c02685408d730631a0f660b165b6
Strider <strange.transistor@gmail.com>
2019-03-03 22:39:45 +0300

    workon Count.

--- start.js
+++ start.js
@@ -4,44 +4,12 @@
 // import upgrade from './upgrade'; upgrade({ colors: false })
 import '.'
 
-var X = { x: 17, x2: 1, y: true, z: [ null, NaN, /xyz/ ] }
-var Y = [ X, X, X ]
+// var X = { x: 17, x2: 1, y: true, z: [ null, NaN, /xyz/ ] }
+// var Y = [ X, X, X ]
 
-console.info(Y)
-console.error(Y)
-console.warn(Y)
-console.debug(Y)
-console.dirxml(Y)
-
-process.exit()
-
-console.group('X')
-
-console.table(
-[
-	{ x: 1, y: 2 },
-	{ y: 2, z: 3 },
-	17,
-	{ x: 1, z: 3 },
-	[ 17 ],
-	[[ 17 ]],
-	{ x: 1, a: [] },
-	Symbol('S'),
-])
-
-console.group('Y')
-
-console.table(
-{
-	1: { x: 1, y: 2 },
-	2: { y: 2, z: 3 },
-	3: 17,
-	10: { x: 1, z: 3 },
-	4: [ 17 ],
-	20: [[ 17 ]],
-	25: { x: 1, a: [] },
-	21: Symbol('S'),
-})
-
-console.group.end()
-console.group.end()
+console.log(1)
+console.count()
+console.count()
+console.count('x')
+console.count()
+console.log(2)
