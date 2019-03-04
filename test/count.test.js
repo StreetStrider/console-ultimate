01e35ef748d90e82ef619c09e20338efca90a164
Strider <strange.transistor@gmail.com>
2019-03-04 21:55:08 +0300

    test count.

---
+++ test/count.test.js
@@ -0,0 +1,64 @@
+
+import it_console from './it-console'
+
+describe('console', () =>
+{
+	it_console(
+	{
+		title: 'count',
+		output: ' ⌚ 1\n ⌚ 2\n ⌚ 3\n',
+		test (console)
+		{
+			console.count()
+			console.count()
+			console.count()
+		},
+	})
+
+	it_console(
+	{
+		isTTY: true,
+		title: 'count',
+		output: ' ⌚ 1\n ⌚ 2\n ⌚ 3\n',
+		test (console)
+		{
+			console.count()
+			console.count()
+			console.count()
+		},
+	})
+
+	it_console(
+	{
+		title: 'label',
+		output: ' ⌚ 1\n ⌚ X 1\n ⌚ Y 1\n ⌚ 2\n ⌚ X 2\n ⌚ 3\n',
+		test (console)
+		{
+			console.count()
+			console.count('X')
+			console.count('Y')
+			console.count()
+			console.count('X')
+			console.count()
+		},
+	})
+
+	it_console(
+	{
+		title: 'reset',
+		output: ' ⌚ 1\n ⌚ X 1\n ⌚ 2\n ⌚ X 2\n ⌚ 1\n ⌚ X 3\n ⌚ 2\n ⌚ X 1\n',
+		test (console)
+		{
+			console.count()
+			console.count('X')
+			console.count()
+			console.count('X')
+			console.count.reset()
+			console.count()
+			console.count('X')
+			console.count.reset('X')
+			console.count()
+			console.count('X')
+		},
+	})
+})
