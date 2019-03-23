7ce1b0ae59cb373cc3f3d8c489146c8e10b7cadc
Strider <strange.transistor@gmail.com>
2019-03-24 00:15:33 +0300

    mv start.

--- test/start.js
+++ test/start.js
@@ -2,7 +2,7 @@
 // import Console from './Console'; var console = Console({ colors: false })
 // import console from './default'
 // import upgrade from './upgrade'; upgrade({ colors: false })
-import '.'
+import '..'
 
 var X = { x: 17, x2: 1, y: true, z: [ null, NaN, /xyz/ ] }
 var Y = [ X, X, X ]
