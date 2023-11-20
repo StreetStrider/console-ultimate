63a6da5fdad02fbbdcaeb765e7d15c16e76d7f4a
Strider <strange.transistor@gmail.com>
2023-11-20 03:45:41 +0300

    thru vs thru1.

--- test/thru.test.js
+++ test/thru.test.js
@@ -6,7 +6,7 @@ describe('thru, as', () =>
 	it_console(
 	{
 		title: 'log.thru',
-		output: ' ⚫ 5\n',
+		output: ' ⚫ 5 0 [ 5 ]\n',
 		test (console)
 		{
 			expect([ 5 ].map(console.log.thru)).deep.eq([ 5 ])
