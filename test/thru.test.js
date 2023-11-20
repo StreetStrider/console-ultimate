3a7bf1584ab112dc312341381b609746d3b49cdc
Strider <strange.transistor@gmail.com>
2023-11-20 03:40:32 +0300

    thru as.

--- test/thru.test.js
+++ test/thru.test.js
@@ -6,20 +6,30 @@ describe('thru, as', () =>
 	it_console(
 	{
 		title: 'log.thru',
-		output: ' ⚫ 1\n',
+		output: ' ⚫ 5\n',
 		test (console)
 		{
-			[ 1 ].map(console.log.thru)
+			expect([ 5 ].map(console.log.thru)).deep.eq([ 5 ])
 		},
 	})
 
 	it_console(
 	{
 		title: 'dir.thru',
-		output: ' ⚫ 1\n',
+		output: ' ⚫ 5\n',
 		test (console)
 		{
-			[ 1 ].map(console.dir.thru)
+			expect([ 5 ].map(console.dir.thru)).deep.eq([ 5 ])
+		},
+	})
+
+	it_console(
+	{
+		title: 'log.as',
+		output: ' ⚫ label 10 5 0 [ 5 ]\n',
+		test (console)
+		{
+			expect([ 5 ].map(console.log.as('label', 10))).deep.eq([ 5 ])
 		},
 	})
 })
